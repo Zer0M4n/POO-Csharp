c90778babfa676ab74848063cd3af8051a198b64
Zer0M4n <123420180+Zer0M4n@users.noreply.github.com>
2023-03-07 08:30:33 -0800

    nuevo programa
    ede

---
+++ UNIDAD_2/EJEMPLOS/EJEMPLO_Sobrecarga_Operadores/EJEMPLO_Sobrecarga_Operadores/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//EJEMPLO DE SOBRECARGA DE OPERADORES
+/*Plantear una clase VectorEnteros que permita crear un vector de 5 elementos 
+  y sobrecargue el operador * de un objeto de la clase VectorEnteros con un valor 
+  de tipo int (el resultado debe ser otro objeto de la clase VectorEnteros donde cada componente se obtiene de
+ multiplicar su valor por el valor entero)*/
+namespace EJEMPLO_Sobrecarga_Operadores
+{
+    internal class Program
+    {
+        class VectorEnteros//VECTORES ENTEROS
+        {
+            private int[] vec;//METODOS
+
+            public VectorEnteros()//CONSTRUCTOR DE LA CLASE
+            {
+                vec = new int[5];
+            }
+
+            public void Cargar()//FUNCION PROCEDIMIENTO
+            {
+                for (int f = 0; f < vec.Length; f++)
+                {
+                    Console.Write("Ingrese componente:");
+                    vec[f] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            public void Imprimir()
+            {
+                for (int f = 0; f < vec.Length; f++)
+                {
+                    Console.Write(vec[f] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            public static VectorEnteros operator *(VectorEnteros v1, int valor)
+            {
+                VectorEnteros resu = new VectorEnteros();
+                for (int f = 0; f < resu.vec.Length; f++)
+                {
+                    resu.vec[f] = v1.vec[f] * valor;
+                }
+                return resu;
+            }
+        }
+            static void Main(string[] args)
+        {
+            VectorEnteros v1 = new VectorEnteros();
+            Console.WriteLine("Carga del vector");
+            v1.Cargar();
+            VectorEnteros vr;
+            Console.WriteLine("Primer Vector");
+            v1.Imprimir();
+            vr = v1 * 10;
+            Console.WriteLine("Vector resultante");
+            vr.Imprimir();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UNIDAD_2/programa18-clase areas/programa18-clase areas/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa18_clase_areas
+{
+    internal class Program
+    {
+        public class AREA//clase AREA
+        {
+            string NOMBRE="";//METODO DE LA CLASSE
+
+            public AREA (string NOMBRE)//CONSTRUCTOR DE LA CLASE
+            {
+             this.NOMBRE = NOMBRE;//CAMPOS DE LA CLASE
+            }
+
+            public double AREA_CUADRADO(double lado)//FUNCION PARA CALCULAR AERA
+            {
+                return Math.Pow(lado, 2);//NOS RETORNA EL VALOR
+            }
+
+            public float AREA_RECTANGULO(float largo   , float ancho)//FUNCION DE CLACULAR AERA RECTANGULO
+            {
+                return largo * ancho;//NOS RETORNA EL VALOR
+            }
+
+            public void DESPLEGARAREA (double AREA)//FUNCION PROCEDIMIENTO
+            {
+                Console.WriteLine("NOMBRE DE LA FIGURA {0}, EL AREA ES {1}  ", NOMBRE,AREA);//IMPRESION
+            }
+            ~AREA()//DESTRUCTOR DE LA CLASE
+            {Console.WriteLine("Objeto Destruido: {0} " , NOMBRE);}
+        }
+        static void Main(string[] args)
+        {
+            string NOMBRE = "";
+            double lado = 0;
+            float largo = 0, ancho = 0;
+
+           //CAPTURAMOS DATOS DEL CUADRADO
+            Console.Write("CUAL ES EL NOMBRE DE LA FIGURA: ");
+            NOMBRE = Console.ReadLine();
+
+            Console.Write("CUANTO MIDE EL LADO DEL CUADRADO: ");
+            lado = double.Parse(Console.ReadLine());
+            
+            AREA CUADRADO = new AREA(NOMBRE);//CONSTRUCTOR DEL OBJETO Cuadrado
+
+            CUADRADO.DESPLEGARAREA(CUADRADO.AREA_CUADRADO(lado));//DESPLEGAR LOS DATOS
+
+            Console.Write("Precione ENTER para continuar");
+            Console.ReadKey();
+            Console.Clear();//Limpiamos pantalla despues de que precione neter
+
+            //Cpaturamos la datos del rectangulo
+            Console.Write("CUAL ES EL NOMBRE DE LA FIGURA: ");
+            NOMBRE = Console.ReadLine();
+            Console.Write("CUANTO MIDE EL LARGO DEL RECTANGULO: ");
+            largo = float.Parse(Console.ReadLine());
+            Console.Write("CUANTO MIDE EL ANCHO DEL RECTANGULO: ");
+            ancho = float.Parse(Console.ReadLine());
+           
+            AREA RECTANGULO = new AREA(NOMBRE);//CONSTRUCTOR DEL OBJETO RECTANGULO
+
+
+            RECTANGULO.DESPLEGARAREA(RECTANGULO.AREA_RECTANGULO(largo,ancho));//DESPLEGAR LOS DATOS
+
+            Console.WriteLine("PRESIONE ENTER PARA SALIR");
+            Console.ReadKey();
+        }
+    }
+}
