4f0049f3d8a4bd5b3c9b4dd3eb534735446e8360
Zer0M4n <123420180+Zer0M4n@users.noreply.github.com>
2023-04-27 11:38:00 -0700

    new

---
+++ C#/UNIDAD_5/programa34-practica_Indice Fuera_Rango/programa34-practica_Indice Fuera_Rango/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa34_practica_Indice_Fuera_Rango
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARAMOS LOS VALORES
+            byte i = 0;
+            //DECLARAMOS VECTOR
+            Random RDM = new Random();
+            int[] VECTOR = new int[15];
+            //GENERAMOS UN TRY-CATCH-FINALLY
+            try
+            {
+                
+
+                //PROCESO
+                //CICLO FOR PARA AGREGAR 10 NUMEROS ALEATARIOS
+                for (i = 0; i < VECTOR.Length; i++)
+                {
+                    VECTOR[i] =  RDM .Next(10,99);
+                }
+                //ARREGLAMOS EL ARREGLO
+                Array.Sort(VECTOR);
+
+                //IMPRIMIMMOS EL VECTOR ORDENADO
+                for(i = 0; i < VECTOR.Length; i++) 
+                {
+                    Console.WriteLine("indice {0}, valor {1} ",i ,VECTOR[i]);
+
+                }
+                Console.ReadKey();
+
+                //GENERAMOS UN ERROR CONTROLADO
+                VECTOR[15] = 12345;
+
+                
+
+            }
+            catch(IndexOutOfRangeException e) 
+            {
+                Console.WriteLine(e.Message);
+                Console.Write("ruta del error : {0}",e.StackTrace);
+            }
+            finally 
+            {
+                Console.WriteLine("fin del programa");
+                Console.WriteLine("El bloque finally siemore se ejecuta");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ C#/UNIDAD_5/programa35-Excepción_División_sobre_Cero/programa35-Excepción_División_sobre_Cero/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa35_Excepción_División_sobre_Cero
+{
+    internal class Program
+    {
+        static float divide(float a, float b)//funcion con la que vamos adividir
+        {
+            if (b == 0) //En el caso que sea 0 lanzamos una excepción
+            {
+                throw new DivideByZeroException();//execipon de division por 0
+            }
+            else
+            {
+                return a / b;//retornal el valor usando division
+            }
+        }
+        static void Main(string[] args)
+        {
+            //declaramos variables
+            float a = 0, b = 0 , RESULTADO = 0;
+
+            try
+            {
+                //Capturamos datos
+                Console.Write("Introduce el primer número: ");
+                a = float.Parse(Console.ReadLine());
+
+                Console.Write("Introduce el segundo número: ");
+                b = float.Parse(Console.ReadLine());
+
+                RESULTADO = divide(a, b);//llammamos ala funcion
+
+                Console.WriteLine("El resultado de la división es: {0}", RESULTADO);//IMPRIMIMOS LA DIVICION
+            }
+            catch(DivideByZeroException m)//EN CASO QUE SE DIVIDA EN 0 SE LLAMAN ESTOS MENSAJE
+            {
+                Console.WriteLine(m.Message);//MENSAJE DE ERROR
+                Console.WriteLine(m.StackTrace);//RUTA DE ERROR
+            }
+            catch(FormatException T)
+            {
+                Console.WriteLine(T.Message);
+                Console.WriteLine(T.StackTrace);
+            }
+            finally//BLOQUE FINALLY
+            {
+                Console.WriteLine("EL BLOQUE FINALLY SIEMPRE SE EJCUTA");
+                Console.WriteLine("Fin del programa precione enter");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
