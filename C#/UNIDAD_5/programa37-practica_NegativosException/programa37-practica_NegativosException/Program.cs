dc3b73d70cf62b4101fb7e0942bd894d4ac224d0
Zer0M4n <123420180+Zer0M4n@users.noreply.github.com>
2023-05-04 08:41:57 -0700

    new

---
+++ C#/UNIDAD_5/programa37-practica_NegativosException/programa37-practica_NegativosException/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa37_practica_NegativosException
+{
+    internal class Program
+    {
+        class NegativeExection : ApplicationException//clase para las exepciones
+        {
+            //Constructor de la clsae que heredA
+            public NegativeExection() : base("No se puede calcular el seno de un numero negativo")
+            {
+                //VACIO
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Declaramos Variables
+            int opc = 0;
+            bool ciclo = true;
+            short Angulo = 0;
+            double Resultado = 0;
+
+
+            try
+            {
+                //Menu
+                do
+                {
+                    Console.WriteLine("Que desea escoger?\n1)Capturar y Calcular Funcion\n2)Salir del programa");
+                    opc = int.Parse(Console.ReadLine());
+                    switch (opc)
+                    {
+                        case 1:
+                            Console.WriteLine("Ingrese el angulo (Numeros enteros)");
+                            Angulo = short.Parse(Console.ReadLine());
+
+                            if (Angulo < 0)
+                            {
+                                throw new NegativeExection();
+                            }
+                            else
+                            {
+
+                                Resultado = Math.Sin(Angulo);
+
+                                Console.WriteLine("El resultado del seno de angulo {0} es: {1} ", Angulo, Resultado);
+                            }
+
+                            Console.WriteLine("Precione enter para Continuar");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Precione enter para salir");
+                            Console.ReadKey();
+                            Console.Clear();
+
+                            ciclo = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("Opcion no valida");
+                            break;
+                    }
+                } while (ciclo == true);
+            }
+            catch (NegativeExection ex) 
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Ruta del error: {0}",ex.StackTrace);
+            }
+            catch(FormatException n)
+            {
+                Console.WriteLine(n.Message);
+                Console.WriteLine("Ruta del error: {0}", n.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("\nEl finally siempre se ejecuta.");
+                Console.WriteLine("\npresione <enter> para continuar...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ C#/UNIDAD_5/programa38 –practica_EcuacionCuadratica/programa38 –practica_EcuacionCuadratica/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa38__practica_EcuacionCuadratica
+{
+    internal class Program
+    {
+         class NEGATIVEexeption : ApplicationException
+        {
+            //Constructor de la clase
+            public NEGATIVEexeption(): base("La Ecuación Cuadrática NO tiene Solución!!!")
+                
+                {
+
+                }
+        }
+        static void Main(string[] args)
+        {
+            //Declaracion de variables
+            int a = 0, b = 0, c = 0, D = 0;
+            int X1 = 0, X2 = 0;
+            char opc = 's';
+            bool cilce = true;
+            try
+            {
+                //CICLO
+                do
+                {
+                    switch (opc)
+                    {
+                        case 'A':
+                            Console.Write("Dame el numero a: ");
+                            a = int.Parse(Console.ReadLine());
+
+                            Console.Write("Dame el numero b: ");
+                            b = int.Parse(Console.ReadLine());
+
+                            Console.Write("Dame el numero c: ");
+                            c = int.Parse(Console.ReadLine());
+
+                            D = (int)(Math.Pow(b, 2) - 4 * a * c);
+
+                            if (D < 0)
+                            {
+                                throw new NEGATIVEexeption();
+
+
+                            }
+                            else
+                            {
+                                X1 = (int)(-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c) / 2 * a);
+                                X2 = (int)(-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c) / 2 * a);
+
+                                Console.WriteLine("El valor de X1 es: {0}", X1);
+                                Console.WriteLine("El valor de X2 es: {0}", X2);
+                            }
+                            break;
+                        case 'B':
+                            Console.WriteLine("Programa terminado, precione enter");
+                            cilce = false;
+                            Console.ReadKey();
+                            break;
+                        default:
+                            Console.WriteLine("Opcion no valida");
+                            break;
+                    }
+                } while (cilce == true);
+                
+                
+            }
+            catch (NEGATIVEexeption Z) 
+            {
+                Console.WriteLine("ERROR ; {0}", Z.Message);
+                Console.WriteLine("RUTA DEL ERROR: {0}",Z.StackTrace);
+            }
+            catch(FormatException n)
+            {
+                Console.WriteLine("ERROR ; {0}", n.Message);
+                Console.WriteLine("RUTA DEL ERROR: {0}",n.StackTrace);
+            }
+            finally 
+            { 
+                Console.WriteLine("“Programa desarrollado por: CESAR GONZALEZ SALAZAR\nNO: 22211575”");
+                Console.WriteLine("Materia: Programacion orientada a objetos");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
